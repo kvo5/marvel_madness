e4eb1d4cffe646dfac9e1dc4d710e77754da7d76
kovyenn <kvo5@uncc.edu>
2025-04-27 03:23:37 -0400

    prep deployment

--- src/app/(board)/page.tsx
+++ src/app/(board)/page.tsx
@@ -3,6 +3,7 @@ import Share from "@/components/Share";
 import Link from "next/link";
 
 const Homepage = () => {
+
   return <div className="">
     <div className='px-4 pt-4 flex justify-between text-textGray font-bold border-b-[1px] border-borderGray'>
       <Link className="pb-3 flex items-center border-b-4 border-iconBlue" href="/">For you</Link>
